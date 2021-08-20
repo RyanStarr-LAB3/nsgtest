381bfeaf9918bd6342190ff7ddd6fc2c7f936a1e
Ryan Starr <ryan.starr@lab3.com.au>
2021-08-20 15:35:03 +1000

    4

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # terraform block
 terraform {
   required_version = ">=1.0"
-#  backend "azurerm" {}
+  #  backend "azurerm" {}
 
   required_providers {
     azurerm = {
@@ -18,12 +18,29 @@ provider "azurerm" {
 
 #create nsg
 resource "azurerm_network_security_group" "nsg1" {
-  name                = "nsgruletest1"
+  name                = "nsgruletest"
   location            = "Australia Southeast"
   resource_group_name = "Ryan-Dev"
 }
 
-# module block. nsg module
-module "nsg" {
-  source = "./modules/nsg"
+#define locals (a csv file of nsg rules)
+locals {
+  nsg_rules = csvdecode(file("./rules.csv"))
+}
+
+#create nsg rules from locals csv file
+resource "azurerm_network_security_rule" "nsg" {
+  count = length(local.nsg_rules)
+
+  name                        = local.nsg_rules[count.index].rule_name
+  priority                    = local.nsg_rules[count.index].priority
+  direction                   = local.nsg_rules[count.index].direction
+  access                      = local.nsg_rules[count.index].access
+  protocol                    = local.nsg_rules[count.index].protocol  
+  source_port_range           = local.nsg_rules[count.index].source_port_range
+  destination_port_range      = local.nsg_rules[count.index].destination_port_range 
+  source_address_prefix       = local.nsg_rules[count.index].source_address_prefix
+  destination_address_prefix  = local.nsg_rules[count.index].destination_address_prefix
+  resource_group_name         = "Ryan-Dev"
+  network_security_group_name = azurerm_network_security_group.nsg1.name
 }
